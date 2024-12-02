e9367677f10e3fe76ce63cb88dfae71ec19d4665
Matyas Fodor <matyas.fodor@gmail.com>
2024-12-02 08:20:13 +0100

    feat: day2 refactor

--- day02/main.ts
+++ day02/main.ts
@@ -2,7 +2,7 @@ import { readFileSync } from "fs";
 import path from "path";
 
 const part1 = true;
-const lines = readFileSync(path.join(import.meta.dirname, "test.txt"), {
+const lines = readFileSync(path.join(import.meta.dirname, "input.txt"), {
   encoding: "utf-8",
 });
 
@@ -17,35 +17,41 @@ const enum State {
   Error,
 }
 
-const resp = reports.filter(
-  (report) =>
-    report.reduce<{ prevElem: null | number; increasing: null | State }>(
-      ({ prevElem, increasing }, elem) => {
-        // First elem
-        if (prevElem === null) {
-          return { prevElem: elem, increasing: null };
-        }
-        // Second elem
-        const diff = elem - prevElem;
-        // if (increasing === null) {
-        //   return {prevElem: elem, increasing: elem > prevElem}
-        // }
-        const absDiff = Math.abs(diff);
-        const safe = 0 < absDiff && absDiff < 4;
-        const currentState = diff < 0 ? State.Decreasing : State.Increasing;
-        return {
-          prevElem: elem,
-          increasing: !safe
-            ? State.Error
-            : increasing === null
-            ? currentState
-            : increasing === currentState
-            ? currentState
-            : State.Error,
-        };
-      },
-      { prevElem: null, increasing: null }
-    ).increasing !== State.Error
-).length;
+const getState = (
+  elem: number,
+  prevElem: number,
+  increasing: State | null
+): State => {
+  const diff = elem - prevElem;
+  const absDiff = Math.abs(diff);
+  const safe = 0 < absDiff && absDiff < 4;
+  const currentState = diff < 0 ? State.Decreasing : State.Increasing;
+  const state = !safe
+    ? State.Error
+    : increasing === null
+    ? currentState
+    : increasing === currentState
+    ? currentState
+    : State.Error;
+  return state;
+};
+
+const resp = reports.filter((report) => {
+  const { diffs } = report.reduce<{ prevElem: number | null; diffs: number[] }>(
+    ({ prevElem, diffs }, elem) => {
+      if (prevElem === null) {
+        return { prevElem: elem, diffs };
+      }
+      return {
+        prevElem: elem,
+        diffs: [...diffs, elem - prevElem],
+      };
+    },
+    { prevElem: null, diffs: [] }
+  );
+  return diffs.every(
+    (e) => e * diffs[0] > 0 && 0 < Math.abs(e) && Math.abs(e) < 4
+  );
+}).length;
 
 console.log(`Number of safe reports ${resp}`);
