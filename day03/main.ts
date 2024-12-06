fc7f95bb71857c3b665e303be17d8a650748ff28
Matyas Fodor <matyas.fodor@gmail.com>
2024-12-06 21:52:56 +0100

    day3 completed

--- day03/main.ts
+++ day03/main.ts
@@ -1,7 +1,7 @@
 import { readFileSync } from "fs";
 import path from "path";
 
-const part1 = true;
+const part1 = false;
 const input = readFileSync(path.join(import.meta.dirname, "input.txt"), {
   encoding: "utf-8",
 });
@@ -9,11 +9,21 @@ const input = readFileSync(path.join(import.meta.dirname, "input.txt"), {
 const re = /mul\((?<left>\d+),(?<right>\d+)\)/g
 const re1 = /mul\((?<left>\d+),(?<right>\d+)\)/
 
-
-const val = input.match(re)?.reduce((acc: number, elem) => {
+const extractMuls = (input: string) => input.match(re)?.reduce((acc: number, elem) => {
   const groups = elem.match(re1)?.groups ?? {};
   const val = parseInt(groups['left']) * parseInt(groups['right']);
   return acc + val;
-}, 0)
+}, 0) ?? 0;
 
-console.log(val)
+if (part1) {
+  console.log(extractMuls(input));
+} else {
+  const splitInput = input.split(/don\'t.*do\(\)/g);
+  console.log(`Split input size ${splitInput.length}`)
+  const val = splitInput.reduce((acc, stringPart) => {
+    return acc + extractMuls(stringPart);
+  }, 0);
+  console.log(val);
+}
+
+// 93252440 - too high
